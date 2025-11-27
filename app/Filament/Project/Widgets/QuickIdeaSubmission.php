e96d18511cfdbc354ab877d8f44264b7dd7a131f
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-11-27 11:01:09 +0500

    Add tenant dashboard quick idea submission widget

--- app/Filament/Project/Widgets/QuickIdeaSubmission.php
+++ app/Filament/Project/Widgets/QuickIdeaSubmission.php
@@ -2,7 +2,6 @@
 
 namespace App\Filament\Project\Widgets;
 
-use App\Models\Idea;
 use App\Models\Project;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -61,7 +60,7 @@ class QuickIdeaSubmission extends Widget implements Forms\Contracts\HasForms
 
     protected function getFormModel(): string
     {
-        return Idea::class;
+        return \stdClass::class;
     }
 
     protected function getFormStatePath(): ?string
@@ -71,31 +70,17 @@ class QuickIdeaSubmission extends Widget implements Forms\Contracts\HasForms
 
     public function submit(): void
     {
-        $host = request()->getHost();
-        $slug = explode('.', $host)[0] ?? null;
+        // Sirf validation + reset, DB me kuch mat save karo
 
-        $project = Project::where('slug', $slug)->firstOrFail();
-
-        $data = $this->form->getState();
-
-        Idea::create([
-            'project_id'   => $project->id,
-            'title'        => $data['title'],
-            'description'  => $data['description'],
-            'team_name'    => $data['team_name'],
-            'pain_score'   => $data['pain_score'],
-            'cost'         => $data['cost'],
-            'duration'     => $data['duration'],
-            'status'       => 'new',
-            'created_by'   => auth()->id(),
-        ]);
+        $this->validate(); // form rules already fields pe lagi hui hain
 
         $this->form->fill();
 
         Notification::make()
             ->title('Idea submitted')
-            ->body('Your idea has been added to the pipeline.')
+            ->body('Your idea has been captured. (Storage not wired yet.)')
             ->success()
             ->send();
     }
+
 }
